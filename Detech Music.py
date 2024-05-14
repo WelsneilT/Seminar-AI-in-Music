328ba830b0030d691b95cf8bb29569826be64b33
WelsneilT <td.tan2711@gmail.com>
2024-05-14 22:55:57 +0700

    がんばろおおお

--- Detech Music.py
+++ Detech Music.py
@@ -2,11 +2,12 @@ import librosa
 import soundfile as sf
 
 # Đọc file âm thanh
-file_path = './Music/justin_cold_water.wav'
+file_path = './Music-Human/Justin-Bieber-Love-Yourself-_PURPOSE-The-Movement_.wav'
 y, sr = librosa.load(file_path)
 
-# Tách riêng giọng hát và âm nhạc
-y_harmonic, y_percussive = librosa.effects.hpss(y)
+# Thử điều chỉnh các tham số margin
+y_harmonic, y_percussive = librosa.effects.hpss(y, margin=(3.0, 3.0))
+
 
 # Lưu giọng hát và âm nhạc thành các file âm thanh riêng biệt
 harmonic_path = 'harmonic_audio.wav'
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import euclidean_distance
-
-point1 = [1, 2]
-point2 = [4, 5]
-
-distance = euclidean_distance(point1, point2)
-print(distance)
